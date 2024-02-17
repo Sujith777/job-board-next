4b35ce8878a842fbcadd4596c222e6a1a44b4790
bspraveen17 <billasujith77@gmail.com>
2024-02-17 23:03:02 +0530

    Add apply button to job details page

--- src/app/jobs/[slug]/page.tsx
+++ src/app/jobs/[slug]/page.tsx
@@ -3,6 +3,7 @@ import { Metadata } from "next";
 import prisma from "@/lib/prisma";
 import { notFound } from "next/navigation";
 import JobDetailsPage from "@/components/JobDetailsPage";
+import { Button } from "@/components/ui/button";
 
 interface PageProps {
   params: { slug: string };
@@ -29,10 +30,26 @@ export async function generateMetadata({
 
 export default async function Page({ params: { slug } }: PageProps) {
   const job = await getJob(slug);
+  const { applicationEmail, applicationUrl } = job;
+
+  const applicationLink = applicationEmail
+    ? `mailto:${applicationEmail}`
+    : applicationUrl;
+  if (!applicationLink) {
+    console.error("Job has no application link or email");
+    notFound();
+  }
 
   return (
     <main className="m-auto my-10 flex max-w-5xl flex-col items-center gap-5 px-3 md:flex-row md:items-start">
       <JobDetailsPage job={job} />
+      <aside>
+        <Button asChild>
+          <a href={applicationLink} className="w-40 md:w-fit">
+            Apply Now
+          </a>
+        </Button>
+      </aside>
     </main>
   );
 }
